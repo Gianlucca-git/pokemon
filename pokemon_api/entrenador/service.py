7efebefcbc5c6e66b1427896c4cc1d0ccc0b56b6
Gianlucca Aguado <gianlucca.aguado@jikkosoft.com>
2021-12-17 20:50:25 -0500

    consumo de la api pokemon, pendiente insertar informacion de los pokemones, logica finalizada.

--- pokemon_api/entrenador/service.py
+++ pokemon_api/entrenador/service.py
@@ -1,4 +1,5 @@
 import requests
+from random import randrange
 
 def generate_request(url, params={}):
     response = requests.get(url, params=params)
@@ -19,3 +20,45 @@ def get_pokemon_list(params={}):
        return pokemonListName
 
     return pokemonListName
+
+def get_pokemon_data(pokemonName):
+    response = generate_request('https://pokeapi.co/api/v2/pokemon/' + pokemonName)
+    
+    pokemonListName = []
+    if response:
+        pokemonListName = response.get('moves')
+    else:
+        return pokemonListName
+
+    pokemonUrlMoves= []
+    i=0
+    listRamdons=[]
+    while i<4:
+        
+        intRamdon= randrange(len(pokemonListName)-1)
+        if intRamdon in listRamdons:
+            continue
+        
+        listRamdons.append(intRamdon)
+        pokemonUrlMoves.append(pokemonListName[listRamdons[i]].get('move').get('url'))
+        
+        i+=1 
+
+    return pokemonUrlMoves
+
+def get_moves_data(url):
+    response = generate_request(url)
+
+    if response:
+
+        nombre = response.get('name')
+        danio  = response.get('accuracy')
+
+        if danio is not None :
+            return [nombre,danio]
+        else:
+            return [nombre,'25']
+    
+    return ['Generico','25']
+
+    
